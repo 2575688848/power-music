bae73ce9dd46bb801fb71fac9a1afed4d6ab5d39
姚太平 <yaotaiping@higohappy.com>
2020-12-14 17:19:39 +0800

    MOD: 增加字体颜色

---
+++ src/main/java/com/ytp/music/utils/TextColorUtils.java
@@ -0,0 +1,39 @@
+package com.ytp.music.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author ytp
+ * @date 2020/12/19
+ */
+@Slf4j
+@Component
+public class TextColorUtils {
+
+    public static void main(String[] args) {
+        System.out.print("\u001b[30m A \u001b[31m B \u001b[32m C \u001b[33m D \u001b[0m");
+        System.out.print("\u001b[34m E \u001b[35m F \u001b[36m G \u001b[37m H \u001b[0m");
+        System.out.print("\u001b[30;1m A \u001b[31;1m B \u001b[32;1m C \u001b[33;1m D \u001b[0m");
+        System.out.print("\u001b[34;1m E \u001b[35;1m F \u001b[36;1m G \u001b[37;1m H \u001b[0m");
+
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                int code = i * 16 + j;
+                System.out.printf("\u001b[38;5;%dm%-4d", code, code);
+            }
+            System.out.println("\u001b[0m");
+        }
+    }
+}
